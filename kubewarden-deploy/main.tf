87d18d319f1e411fc857dc095f95d404c8b29a7e
Zain <zain.marvi@rackspace.com>
2024-10-30 10:09:45 -0500

    Update main.tf

--- kubewarden-deploy/main.tf
+++ kubewarden-deploy/main.tf
@@ -1,5 +1,5 @@
 resource "rancher2_catalog_v2" "rancher-kubewarden-charts" {
-  cluster_id = "c-m-8kvhvk2t"
+  cluster_id = "<CLUSTER_ID>"
   name = "kubewarden-charts"
   url = "https://charts.kubewarden.io"
 }
@@ -16,7 +16,7 @@ resource "rancher2_app_v2" "rancher-kubewarden-controller" {
 }
 
 resource "rancher2_app_v2" "rancher-kubewarden-defaults" {
-  cluster_id = "c-m-8kvhvk2t"
+  cluster_id = "<CLUSTER_ID>"
   name = "kubewarden-defaults"
   namespace = "cattle-kubewarden-system"
   repo_name = "kubewarden-charts"
